1c78307c25bb2da0c85c2a40ea6acc38b393ca8d
Solann PUYGRENIER <spuygrenier@cfai-formation.com>
2021-09-26 22:53:49 +0200

    remove file from wrong location

--- src/app/national-phone-number.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NationalPhoneNumberPipe } from './national-phone-number.pipe';
-
-describe('NationalPhoneNumberPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NationalPhoneNumberPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
